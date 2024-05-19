4f21de1bae0fc55028f2ec2778cb2adea63d7de3
QuantumRiddle <samurai-1-02@mail.ru>
2024-05-19 03:41:32 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Задание_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_question = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1/10";
+            // 
+            // text_question
+            // 
+            this.text_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text_question.Location = new System.Drawing.Point(37, 100);
+            this.text_question.Name = "text_question";
+            this.text_question.Size = new System.Drawing.Size(494, 99);
+            this.text_question.TabIndex = 1;
+            this.text_question.Text = "Вопрос\r\n";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(41, 214);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Answer1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(41, 240);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Answer2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(41, 266);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(76, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Answer4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(41, 292);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 20);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Answer3";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(458, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.text_question);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label text_question;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Задание 1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_1
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+        int seclect = 0;
+        int right = 0;
+        int[] right_answ = {3,4,2,4,2,2,1,3,2,2,1,3,2,4,4};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            next(n);
+        }
+        
+
+        public void next (int n)
+        {   
+            
+            label1.Text = (n+1) + "/15";
+
+            string[] right_answers = {
+                "Условное обозначение данных", "Java", "equals()", "if", "private", "Сокрытие деталей реализации", "map()", "PostgreSQL", "GET", "Quick sort", "Singleton", "new", "React", "[1, 2, 3, 4, 5]", @"""1020"""
+            };
+
+            ;
+            string[] questions = {
+                "Что такое переменная в программировании?",
+                "Какой язык программирования является строго типизированным?",
+                "Какой метод в Java используется для сравнения строк?",
+                "Что из следующего не является циклом?",
+                "Какой спецификатор доступа в Java делает члены класса доступными только внутри того же класса?",
+                "Что такое инкапсуляция?",
+                "Какая функция JavaScript создает новый массив, применяя заданную функцию к каждому элементу исходного массива?",
+                "Какая база данных является реляционной?",
+                "Какой HTTP метод используется для получения данных с сервера?",
+                "Что из перечисленного является алгоритмом сортировки?",
+                "Какой паттерн проектирования подразумевает создание одного экземпляра класса?",
+                "Какое ключевое слово в C# используется для создания экземпляра объекта?",
+                "Какая библиотека в JavaScript часто используется для построения пользовательских интерфейсов?",
+                "Как в Python создать список чисел от 1 до 5?",
+                "Чему равна переменная x после выполнения следующего кода JavaScript: var x = 10 + '20';?"
+            };
+
+
+            string[,] questionsAndAnswers = new string[,]
+            {
+                {"Инструкция", "Устройство хранения данных", "Условное обозначение данных", "Тип данных"},
+                {"Python", "JavaScript", "PHP", "Java"},
+                {"compare()", "equals()", "isEqual()", "compareTo()"},
+                {"for", "while", "do-while", "if"},
+                {"public", "private", "protected", "default"},
+                {"Процесс перенаправления вызова функции", "Сокрытие деталей реализации", "Механизм наследования классов", "Разделение программы на модули"},
+                {"map()", "filter()", "reduce()", "forEach()"},
+                {"MongoDB", "Redis", "PostgreSQL", "Cassandra"},
+                {"POST", "GET", "PUT", "DELETE"},
+                {"Binary search", "Quick sort", "First-in-first-out (FIFO)", "Secure Socket Layer (SSL)"},
+                {"Singleton", "Adapter", "Observer", "Decorator"},
+                {"struct", "class", "new", "this"},
+                {"jQuery", "React", "Lodash", "Axios"},
+                {"list(1, 5)", "[1, 5]", "range(1, 6)", "[1, 2, 3, 4, 5]"},
+                {"30", "1020", "30", @"""1020"""}
+            };
+
+            text_question.Text = questions[n];
+            radioButton1.Text = questionsAndAnswers[n, 0];
+            radioButton2.Text = questionsAndAnswers[n, 1];
+            radioButton3.Text = questionsAndAnswers[n, 2];
+            radioButton4.Text = questionsAndAnswers[n, 3];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            if (seclect == right_answ[n])
+            {
+                right++;
+            }
+            if (n < 14)
+            {
+                n++;
+                next(n);
+            }
+            else
+            {
+                MessageBox.Show(text: "Вы набрали " + right + "/15 баллов!");
+                n = 0;
+                right = 0;
+                next(n);
+
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            seclect = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            seclect = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            seclect = 3;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            seclect = 4;
+        }
+
+
+    }
+}
